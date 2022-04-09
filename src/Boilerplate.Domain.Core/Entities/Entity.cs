a560d5f74f9e710b82b499e11faa7e4be3c04f43
Matheus Valle <55810334+mtheusvalle@users.noreply.github.com>
2022-04-09 16:32:26 -0300

    Initial commit

---
+++ src/Boilerplate.Domain.Core/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boilerplate.Domain.Core.Entities
+{
+    public abstract class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
